4148076bf2eee3975e48495f922f353cba4c2371
Zachary Newton <zacharyrnewton@gmail.com>
2019-01-07 00:18:53 -0500

    Made fixes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,32 +1,7 @@
-$(window).on({
-  load:function(){
-   if (window.matchMedia("(min-width: 800px)").matches) {
-     $( "img#name-image" ).hide();
-     $( "span#name" ).hover(
-       function() {
-         $( "img#name-image" ).show();
-       }, function() {
-         $( "img#name-image" ).hide();
-       }
-     );
-   } else {
-     $( "img#name-image" ).show();
-   };
- }
-});
-$(window).on({
-  resize:function(){
-   if (window.matchMedia("(min-width: 800px)").matches) {
-     $( "img#name-image" ).hide();
-     $( "span#name" ).hover(
-       function() {
-         $( "img#name-image" ).show();
-       }, function() {
-         $( "img#name-image" ).hide();
-       }
-     );
-   } else {
-     $( "img#name-image" ).show();
-   };
- }
-});
+$( "span#hero-name" ).hover(
+  function() {
+    $( "img#name-image" ).show();
+  }, function() {
+    $( "img#name-image" ).hide();
+  }
+);
