d2da871802ac437f6d4cdc2ef30008be4bb3019b
Zachary Newton <zacharyrnewton@gmail.com>
2019-01-06 20:32:37 -0500

    Updated mobile

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,32 @@
-$( "span#name" ).hover(
-  function() {
-    $( "img#name-image" ).show();
-  }, function() {
-    $( "img#name-image" ).hide();
-  }
-);
+$(window).on({
+  load:function(){
+   if (window.matchMedia("(min-width: 800px)").matches) {
+     $( "img#name-image" ).hide();
+     $( "span#name" ).hover(
+       function() {
+         $( "img#name-image" ).show();
+       }, function() {
+         $( "img#name-image" ).hide();
+       }
+     );
+   } else {
+     $( "img#name-image" ).show();
+   };
+ }
+});
+$(window).on({
+  resize:function(){
+   if (window.matchMedia("(min-width: 800px)").matches) {
+     $( "img#name-image" ).hide();
+     $( "span#name" ).hover(
+       function() {
+         $( "img#name-image" ).show();
+       }, function() {
+         $( "img#name-image" ).hide();
+       }
+     );
+   } else {
+     $( "img#name-image" ).show();
+   };
+ }
+});
